7ff82530b3e835c8f865fcbad76c6f8c2c345664
Mark Vestil <mvestil@deputy.com>
2021-03-03 10:00:27 +1100

    use redis cluster config

--- config/database.php
+++ config/database.php
@@ -121,10 +121,21 @@ return [
 
         'client' => env('REDIS_CLIENT', 'phpredis'),
 
-        'options' => [
+        'clusters' => [
+            'default' => [
+                [
+                    'host' => env('REDIS_HOST', 'localhost'),
+                    'password' => env('REDIS_PASSWORD', null),
+                    'port' => env('REDIS_PORT', 6379),
+                    'database' => 0,
+                ],
+            ],
+        ],
+
+        /*'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
-        ],
+        ],*/
 
         'default' => [
             'url' => env('REDIS_URL'),
