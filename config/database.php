1c2ce676c964c8a57646647dc866bed338f64235
Mark Vestil <mvestil@deputy.com>
2021-03-03 21:23:12 +1100

    try removing the default settings

--- config/database.php
+++ config/database.php
@@ -137,13 +137,13 @@ return [
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
         ],*/
 
-        'default' => [
+        /*'default' => [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
-        ],
+        ],*/
 
         'cache' => [
             'url' => env('REDIS_URL'),
